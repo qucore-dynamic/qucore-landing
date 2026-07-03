8f12c2c681e1af1fd33e393775de5bea9259d679
Artem Zhyto <artemzhr11@gmail.com>
2026-07-03 17:17:37 +0300

    removed the `isFirstScreen` parameter due to a change in the widget animation

--- src/components/shared/FadeUpMotion.tsx
+++ src/components/shared/FadeUpMotion.tsx
@@ -9,30 +9,19 @@ type Props = {
   x?: number
   y?: number
   className?: string
-  isFirstScreen?: boolean
 }
 
-const FadeUpMotion: React.FC<Props> = ({
-  children,
-  delay = 0,
-  x,
-  y,
-  className,
-  isFirstScreen = false,
-}) => {
+const FadeUpMotion: React.FC<Props> = ({ children, delay = 0, x, y, className }) => {
   const initial = {
     opacity: 0,
     ...(x !== undefined ? { x } : {}),
     ...(y !== undefined ? { y } : {}),
   }
 
-  const target = { opacity: 1, x: 0, y: 0 }
-
   return (
     <motion.div
       initial={initial}
-      animate={isFirstScreen ? target : undefined}
-      whileInView={!isFirstScreen ? target : undefined}
+      whileInView={{ opacity: 1, x: 0, y: 0 }}
       viewport={{ once: true }}
       transition={{ duration: 0.75, delay }}
       className={className}
